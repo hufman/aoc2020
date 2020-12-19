f3d11b07d1ed28fa20784fbf8030a625025cb1dd
Walter Huf <hufman@gmail.com>
2020-12-18 16:45:47 -0800

    Implements day6b

--- src/day6.rs
+++ src/day6.rs
@@ -15,3 +15,23 @@ pub fn solve_part1(blocks: &[String]) -> u32 {
          .count() as u32
     }).sum()
 }
+
+#[aoc(day6, part2)]
+pub fn solve_part2(blocks: &[String]) -> u32 {
+    blocks.iter().map(|b: &String| {
+        let responses = b.lines()
+         .map(|l| l.chars().collect())
+         .fold1(|l: Vec<char>, r: Vec<char>| common_chars(&l, &r)).unwrap();
+        println!("{:?} -> {}", responses, responses.len());
+        responses
+         .len() as u32
+    }).sum()
+}
+
+fn common_chars(l: &Vec<char>, r: &Vec<char>) -> Vec<char> {
+    // find the common elements between two lists of chars
+    l.iter().sorted().merge(r.iter().sorted())
+     .tuple_windows()
+     .filter_map(|(&l, &r)| if l == r { Some(l) } else { None })
+     .collect()
+}
